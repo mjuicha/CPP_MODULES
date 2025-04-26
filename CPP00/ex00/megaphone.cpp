b9597788a4fae6c514b398930242da87255152f6
Mohamed amine Juicha <mjuicha@e2r9p15.1337.ma>
2025-04-26 17:32:16 +0100

    refactoring: good version of megaphone

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -4,23 +4,19 @@
 
 int main(int ac, char **av)
 {
-    int i = 1;
-    int j = 0;
     if (ac == 1)
+    {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    }
     else
     {
-        while (i + 1 <= ac)
+        for(int i = 1; i < ac; ++i)
         {
-            j = 0;
-            while (av[i] && av[i][j])
+            std::string str = av[i];
+            for (size_t j = 0; j < str.length(); j++)
             {
-                if (islower(av[i][j]))
-                    av[i][j] = toupper(av[i][j]);
-                std::cout << av[i][j];
-                j++;
+                std::cout << (char)std::toupper(str[j]);
             }
-            i++;
         }
     }
     std::cout << std::endl;
